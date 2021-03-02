40bc931450d3a2c171f597a70a39493e5ae03453
Breanna Starika <starikabreanna@gmail.com>
2021-03-02 16:25:05 -0700

    Updated 3/2

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,18 @@
+alert("Hello, World!"); //utilizing alert() method
+window.alert("My name is Breanna");
+document.write("I am creating prompts using various methods.  ");
+var B="This is a string.   ";
+var B=B.fontcolor("red");
+document.write(B);
+document.write("We will now test quotations, \"Testing one,two,three.\"        "); // expressing quotations 
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss.   "); //Concatenating strings with the + operator
+var A="Learning how to concatenate  " + "a string.   "
+document.write(A);
+var dogs = "The Dog Fam", First='Mochi', Second='Ziah', Third='Loki';
+document.write(First);
+var blues="  I have the blues.";
+var blues=blues.fontcolor("blue"); //Assigning color to variable
+document.write(blues);
+document.write(3+3);
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Javascript-Projects
 Projects completed through The Tech Academy
-Demonstrated use of strings, pbjects, functions, methods, numbers, symbols, and booleans.
-Coded external links with src attribute, and alert prompts.
+Demonstrated use of strings, objects, functions, methods, numbers, symbols, expressions and booleans.
+Coded external links with src attribute.
+Assigned variables to values that can be altered, strings, etc.
+Concatenating strings with the + operator.
+Utilized various methods () including alert(), write(), etc.
+Expressing quotations with \".
 Can differentiate between arguments and parameters when creating subprograms.
